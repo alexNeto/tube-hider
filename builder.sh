35eca73585e9cb3e1989c0fad1e21f8065101109
alexNeto <58237299+alexNeto@users.noreply.github.com>
2021-11-06 17:11:23 -0300

    Update build script

--- builder.sh
+++ builder.sh
@@ -2,9 +2,16 @@ _file_name="tube-hider-"
 _file_extenssion=".zip"
 _file_version=$(cat addon/manifest.json | grep version | tail -n 1 | tr -d ' a-z:,\"')
 
+_build_directory=""
 
-mkdir -p builds
+if [[ $1 == "--release" ]]; then
+    _build_directory="releases"
+else
+    _build_directory="builds"
+fi
+
+mkdir -p $_build_directory
 cd addon
-zip -r -FS ../builds/$_file_name$_file_version$_file_extenssion * --exclude '*.git'
+zip -r -FS ../$_build_directory/$_file_name$_file_version$_file_extenssion * --exclude '*.git'
 cd ..
 echo $_file_name$_file_version$_file_extenssion completed!
